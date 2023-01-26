9b892b0b1d6f78f9686344c4d781260de330cf73
turan <turan6677@gmail.com>
2023-01-26 23:34:21 +0300

    Constant file created.

--- CurrencyApp-Case/Constant/Constant.swift
+++ CurrencyApp-Case/Constant/Constant.swift
@@ -7,3 +7,26 @@
 
 import Foundation
 
+enum currenciesAPICall: String {
+    
+    private var baseUrl: String {
+        "https://psp-merchantpanel-service-sandbox.ozanodeme.com.tr/api/v1/dummy/coins"
+    }
+    
+    case getCurrencies
+    
+    private var urlString: String {
+        
+        switch self {
+        case.getCurrencies:
+            return "\(baseUrl)"
+        }
+    }
+    
+    var url: URL {
+        switch self {
+        case.getCurrencies:
+            return URL (string: "\(baseUrl)")!
+        }
+    }
+}
